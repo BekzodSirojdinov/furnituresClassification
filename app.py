41d129251698dcb2bffc8c078203c0eaaae2f320
Bekzod Sirojdinov <bekzod.sirojdinov@gmail.com>
2024-03-12 14:54:53 +0100

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title('Furnitureni klassifikatsiya qiluvchi model (Bed, Couch, Table)')
+
+#rasmni joylash
+
+file=st.file_uploader('Rasm yuklash ', type=['png','jpeg','jpg'])
+if file:
+    st.time_input("Prediction Time")
+    st.image(file)
+    #PIL convert
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner('furniture_model.pkl')
+
+    #prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
+
+    #plotting
+    fig=px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)    
